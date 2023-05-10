d07f3a1cc86309b02439c70411e938b81f33993e
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-10 10:11:59 +0200

    added variables

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_storage_container" "res-8" {
+  name                 = "content"
+  storage_account_name = "sttvxbgsftoyot5"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage account name"
+}
