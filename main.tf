be72c2544ccc93b81717682d67505e2a7304702b
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2023-05-10 11:23:58 +0200

    fixed naming convention

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_container" "storage_countainer" {
-  name                 = var.name
+  name                 =  lower(format("sc%s%s%s", var.project, var.environment, var.name))
+
   storage_account_name = var.storage_account_name
 }
